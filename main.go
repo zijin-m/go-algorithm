7a477ea6e0f70298fe7f4b6f823ac6f4fc63b03a
ethan <meshethan@gmail.com>
2022-04-16 23:31:13 +0800

    feat: heap

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import "container/heap"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
 
 func main() {
-	s := []int{5}
-	s = append(s, 7)
-	s = append(s, 9)
-	x := append(s, 11)
-	y := append(s, 12)
-	fmt.Println(s, x, y)
+	h := &IntHeap{2, 1, 5}
+	heap.Init(h)
+	heap.Push(h, 3)
+	heap.Pop(h)
 }
