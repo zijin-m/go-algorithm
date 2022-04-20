12854cb5b5c4f633c69c05289a16a3c0906d1e58
ethan <meshethan@gmail.com>
2022-04-20 19:34:55 +0800

    feat: lcs 最长公共子串

---
+++ coding/lcs.go
@@ -0,0 +1,31 @@
+package coding
+
+func LongestCommonSubsequence(text1 string, text2 string) int {
+	if len(text1) == 0 || len(text2) == 0 {
+		return 0
+	}
+	m := len(text1)
+	n := len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 0; i <= len(text1); i++ {
+		for j := 0; j <= len(text2); j++ {
+			if i == 0 || j == 0 {
+				dp[i][j] = 0
+				continue
+			}
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				if dp[i-1][j] > dp[i][j-1] {
+					dp[i][j] = dp[i-1][j]
+				} else {
+					dp[i][j] = dp[i][j-1]
+				}
+			}
+		}
+	}
+	return dp[m][n]
+}
