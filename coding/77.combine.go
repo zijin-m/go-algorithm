cd448233778123e414fb3aef0d5075cea2688f3c
Ethan <meshethan@gmail.com>
2022-04-21 17:22:52 +0800

    ✨ feat(lc.77): 组合

---
+++ coding/77.combine.go
@@ -0,0 +1,26 @@
+package coding
+
+var res [][]int
+var path []int
+
+func Combine(n int, k int) [][]int {
+	res = [][]int{}
+	path = []int{}
+	backtracing(n, k, 1)
+	return res
+}
+
+func backtracing(n, k, start int) {
+	if len(path) == k {
+		// ! 重新创建切片，避免使用同样的地址，造成值被覆盖
+		temp := make([]int, k)
+		copy(temp, path)
+		res = append(res, temp)
+		return
+	}
+	for i := start; i <= n; i++ {
+		path = append(path, i)
+		backtracing(n, k, i+1)
+		path = path[:len(path)-1]
+	}
+}
