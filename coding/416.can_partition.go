e896259d543b2ad546e493ef86e7a77af0f84cce
Ethan <meshethan@gmail.com>
2022-04-22 15:19:36 +0800

    feat: 一维数组优化

--- coding/416.can_partition.go
+++ coding/416.can_partition.go
@@ -10,22 +10,17 @@ func CanPartition(nums []int) bool {
 	}
 	target := sum / 2
 	n := len(nums)
-	dp := make([][]bool, n)
-	for i := 0; i < n; i++ {
-		dp[i] = make([]bool, target+1)
-	}
-	if nums[0] < target {
-		dp[0][nums[0]] = true
-	}
+	dp := make([]int, target+1)
 	for i := 1; i < n; i++ {
-		v := nums[i]
-		for j := 0; j <= target; j++ {
-			if j < v {
-				dp[i][j] = dp[i-1][j]
-			} else {
-				dp[i][j] = dp[i-1][j] || dp[i-1][j-v]
-			}
+		for j := target; j >= nums[i]; j-- {
+			dp[j] = max(dp[j], dp[j-nums[i]]+nums[i])
 		}
 	}
-	return dp[n-1][target]
+	return dp[target] == target
+}
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
 }
