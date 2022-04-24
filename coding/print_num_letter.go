3b4c763aa8250f4c9c4b2d56ae0402d4496d55fe
ethan <meshethan@gmail.com>
2022-04-24 16:13:25 +0800

    feat: 更新交替打印的实现

--- coding/print_num_letter.go
+++ coding/print_num_letter.go
@@ -8,39 +8,38 @@ import (
 // 使用两个 goroutine 交替打印序列，一个 goroutine 打印数字， 另外一个 goroutine 打印字母
 // 最终效果如下：12AB34CD56EF78GH910IJ1112KL1314MN1516OP1718QR1920ST2122UV2324WX2526YZ2728
 func PrintNumLetter() {
-	var (
-		num    = make(chan struct{})
-		letter = make(chan struct{})
-		wg     = sync.WaitGroup{}
-	)
+	num, letter := make(chan struct{}), make(chan struct{})
+	var wg sync.WaitGroup
 	wg.Add(2)
 	go func() {
+		defer wg.Done()
 		i := 1
-		for {
-			if i > 28 {
-				wg.Done()
-				return
-			}
-			<-num
-			fmt.Printf("%d%d", i, i+1)
-			i = i + 2
+		for range num {
+			fmt.Print(i)
+			i++
+			fmt.Print(i)
+			i++
 			letter <- struct{}{}
 		}
 	}()
 
 	go func() {
-		c := 'A'
-		for {
-			<-letter
-			if c >= 'Z' {
-				wg.Done()
-				return
+		defer wg.Done()
+		i := 'A'
+		for range letter {
+			if i > 'Z' {
+				close(num)
+				break
 			}
-			fmt.Printf("%s%s", string(c), string(c+1))
-			c = c + 2
+			fmt.Print(string(i))
+			i++
+			fmt.Print(string(i))
+			i++
 			num <- struct{}{}
 		}
 	}()
+
 	num <- struct{}{}
 	wg.Wait()
+
 }
